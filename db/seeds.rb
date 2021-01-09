5eb8ccec01d2b6671c9326eb851c3646e29d9223
Oscar Icochea Calenzani <o.icochea09@gmail.com>
2021-01-09 13:28:58 -0500

    seeded db

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,9 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+Post.create ({title:"test1", date: "1/9/2021", body: "Test Body ", img:"https://i.imgur.com/64ovFBc.png"})
+Post.create ({title:"test2", date: "1/10/2021", body: "Test Body ", img:"https://i.imgur.com/64ovFBc.png"})
+Post.create ({title:"test3", date: "1/7/2021", body: "Test Body ", img:"https://i.imgur.com/64ovFBc.png"})
+
+p Post.all
